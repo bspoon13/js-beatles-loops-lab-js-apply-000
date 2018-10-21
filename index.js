0d0356bb916b861bd12cfdaf543f00b446fb0692
Billy Witherspoon <wwither@gmail.com>
2018-10-21 21:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+var theBeatlesPlay = function(musician, instrument) {
+  var arr = [];
+  for (var i = 0; i < musician.length; i++){
+   var action = (musician[i] + " plays " + instrument[i]);
+   arr[i] = (action);
+  }
+  console.log(arr);
+  return arr;
+}
+
+var johnLennonFacts = function(facts) {
+  var n = 0;
+  while (n < facts.length){
+    facts[n] = (facts[n] + "!!!");
+    n++;
+  }
+  console.log(facts);
+  return facts;
+}
+
+var iLoveTheBeatles = function(number) {
+ var statement = [];
+ var j = 0;
+ do {
+   statement[j] = ('I love the Beatles!')
+   j++;
+ } while (number != 17);
+console.log(statement);
+return (statement);
+}
+
+
